e8f315c8bb6fc847f57abca125badb829fd0e1db
ronalynbaniagas <baniagasronalyn@gmail.com>
2024-09-14 15:57:39 +0800

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,72 @@
+let firstFavNumb = 20;
+let secondFavNumb = 13;
+
+console.log("USING GREATER THAN");
+let c = 20 > 13;
+console.log("My first favorite number is", firstFavNumb);
+console.log("My second favorite number is", secondFavNumb);
+console.log("20 > 13");
+console.log(firstFavNumb > secondFavNumb);
+
+console.log("USING LESS THAN");
+let O = 20 < 13;
+console.log("My first favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("20 < 13");
+console.log(firstFavNumb < secondFavNumb);
+
+console.log("USING GREATER THAN OR EQUAL TO");
+let N = 20 >= 14;
+console.log("My first favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("20 >= 13");
+console.log(firstFavNumb >= secondFavNumb);
+
+console.log("USING LESS THAN OR EQUAL TO");
+let S = 20 <= 13;
+console.log("My first favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("20 <= 13");
+console.log(firstFavNumb <= secondFavNumb);
+
+console.log("USING STRICTLY EQUALITY");
+let A = 20 === 13;
+console.log("My first favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("20 === 13");
+console.log(firstFavNumb === secondFavNumb);
+
+console.log("USING LOOSE EQUALITY");
+let U = 20 == 13;
+console.log("My first favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("20 == 13");
+console.log(firstFavNumb == secondFavNumb);
+
+console.log("USING STRICT INEQUALITY");
+let Z = 20 !== 13;
+console.log("My first favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("20 !== 13");
+console.log(firstFavNumb !== secondFavNumb);
+
+console.log("USING LOOSE INEQUALITY");
+let L = 20 != 13;
+console.log("My first favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("20 != 13");
+console.log(firstFavNumb != secondFavNumb);
+
+console.log("USING AND");
+let R = 20 && 13;
+console.log("My first favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("20 && 13");
+console.log(firstFavNumb && secondFavNumb);
+
+console.log("USING OR");
+let M = 20 != 13;
+console.log("My first favorite number is" , firstFavNumb);
+console.log("My second favorite number is" , secondFavNumb);
+console.log("20 != 13");
+console.log(firstFavNumb != secondFavNumb);
